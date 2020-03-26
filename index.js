723c6aec298f9ee4c26875c4cb3f696a3357f942
nuga09 <seph_sky@yahoo.com>
2020-03-26 15:47:00 +0100

    added api document

--- index.js
+++ index.js
@@ -17,11 +17,3 @@ app.listen(PORT, () => {
 // [END gae_node_request_example]
 
 module.exports = app;
-
-var express = require('express')
-var app = express()
-
-// respond with "Hello, this route should be excluded from Stackdriver logging." when a GET request is made to the homepage
-app.get('/stealth-Hello', function (req, res) {
-  res.send("Hello, this route should be excluded from Stackdriver logging.")
-})
